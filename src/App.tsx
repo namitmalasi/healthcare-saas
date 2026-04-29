9f6594946f11283d9e24c0d56b3498b74e06c607
Namit Malasi <namitmalasi@gmail.com>
2026-04-29 12:16:11 +0530

    minor change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Analytics from "./pages/Analytics";
