62358246cd735cdc3a8d215193cd40bc0696c8f8
Namit Malasi <namitmalasi@gmail.com>
2026-04-29 12:19:55 +0530

    minor change

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", async () => {
-    try {
-      const registration = await navigator.serviceWorker.register("/sw.js");
-      console.log("Service Worker registered:", registration.scope);
-    } catch (error) {
-      console.error("SW registration failed:", error);
-    }
-  });
-}
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
